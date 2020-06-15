ddbea4113ca1a33e11a0798b0534ef4b798f3cc3
xashru <xashru@gmail.com>
2020-06-16 05:36:42 +0600

    added CRF model

--- src/model.py
+++ src/model.py
@@ -1,6 +1,7 @@
 import torch.nn as nn
 import torch
 from config import *
+from torchcrf import CRF
 
 
 class DeepPunctuation(nn.Module):
@@ -28,3 +29,41 @@ class DeepPunctuation(nn.Module):
         x = torch.transpose(x, 0, 1)
         x = self.linear(x)
         return x
+
+
+class DeepPunctuationCRF(nn.Module):
+    def __init__(self, pretrained_model, freeze_bert=False):
+        super(DeepPunctuationCRF, self).__init__()
+        self.output_dim = len(punctuation_dict)
+        self.bert_layer = MODELS[pretrained_model][0].from_pretrained(pretrained_model)
+        # Freeze bert layers
+        if freeze_bert:
+            for p in self.bert_layer.parameters():
+                p.requires_grad = False
+        bert_dim = MODELS[pretrained_model][2]
+        self.lstm = nn.LSTM(input_size=bert_dim, hidden_size=bert_dim, num_layers=1, bidirectional=True)
+        self.linear = nn.Linear(in_features=bert_dim * 2, out_features=len(punctuation_dict))
+        self.crf = CRF(len(punctuation_dict), batch_first=True)
+
+    def forward(self, x, attn_masks, y):
+        if len(x.shape) == 1:
+            x = x.view(1, x.shape[0])  # add dummy batch for single sample
+        # (B, N, E) -> (B, N, E)
+        x = self.bert_layer(x, attention_mask=attn_masks)[0]
+        # (B, N, E) -> (N, B, E)
+        x = torch.transpose(x, 0, 1)
+        x, (_, _) = self.lstm(x)
+        # (N, B, E) -> (B, N, E)
+        x = torch.transpose(x, 0, 1)
+        x = self.linear(x)
+        # CRF requires byte tensor for mask
+        attn_masks = attn_masks.byte()
+        crf_out = self.crf.forward(x, y, mask=attn_masks, reduction='token_mean')
+        y_pred = None
+        # pad list of integers to have equal length and convert to torch tensor
+        if not self.training:
+            dec_out = self.crf.decode(x, mask=attn_masks)
+            y_pred = torch.zeros(y.shape).long().to(y.device)
+            for i in range(len(dec_out)):
+                y_pred[i, :len(dec_out[i])] = torch.tensor(dec_out[i]).to(y.device)
+        return crf_out, y_pred
