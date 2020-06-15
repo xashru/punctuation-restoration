21c84a44634ecb9096901729877e217aa4ffee6a
xashru <xashru@gmail.com>
2020-06-16 05:37:31 +0600

    test code updated to match with training

--- src/test.py
+++ src/test.py
@@ -2,17 +2,14 @@ import os
 import torch
 import torch.nn as nn
 import numpy as np
-from torch.utils import data
 import torch.multiprocessing
-from transformers import XLMRobertaTokenizer
 
 from argparser import parse_arguments
 from dataset import Dataset
 from model import DeepPunctuation
 from config import *
 
-
-torch.multiprocessing.set_sharing_strategy('file_system')   # https://github.com/pytorch/pytorch/issues/11201
+torch.multiprocessing.set_sharing_strategy('file_system')  # https://github.com/pytorch/pytorch/issues/11201
 
 args = parse_arguments()
 
@@ -23,31 +20,27 @@ torch.backends.cudnn.benchmark = False
 np.random.seed(args.seed)
 
 # tokenizer
-if args.pretrained_model == 'xlm-roberta-base':
-    tokenizer = XLMRobertaTokenizer.from_pretrained("xlm-roberta-base")
-elif args.pretrained_model == 'xlm-roberta-large':
-    tokenizer = XLMRobertaTokenizer.from_pretrained("xlm-roberta-large")
-else:
-    raise ValueError('Incorrect pretrained_model argument')
-
+tokenizer = MODELS[args.pretrained_model][1].from_pretrained(args.pretrained_model)
+token_style = MODELS[args.pretrained_model][3]
 
 test_files = os.listdir(os.path.join(args.data_path, 'test'))
 test_set = []
 for file in test_files:
-    test_set.append(Dataset(os.path.join(args.data_path, 'test', file), tokenizer, args.sequence_length))
+    test_set.append(Dataset(os.path.join(args.data_path, 'test', file), tokenizer, args.sequence_length, token_style,
+                            0, False))
 
 # Data Loaders
 data_loader_params = {
     'batch_size': args.batch_size,
-    'shuffle': True,
-    'num_workers': 4
+    'shuffle': False,
+    'num_workers': 0
 }
 
-test_loaders = [data.DataLoader(x, **data_loader_params) for x in test_set]
+test_loaders = [torch.utils.data.DataLoader(x, **data_loader_params) for x in test_set]
 
 # logs
 model_save_path = os.path.join(args.save_path, 'weights.pt')
-log_path = os.path.join(args.save_path, args.name + '_logs_all.txt')
+log_path = os.path.join(args.save_path, args.name + '_logs_test.txt')
 
 # Model
 device = torch.device('cuda' if (args.cuda and torch.cuda.is_available()) else 'cpu')
@@ -64,9 +57,9 @@ def test(data_loader):
     num_iteration = 0
     deep_punctuation.eval()
     # +1 for overall result
-    tp = np.zeros(1+len(punctuation_dict), dtype=np.int)
-    fp = np.zeros(1+len(punctuation_dict), dtype=np.int)
-    fn = np.zeros(1+len(punctuation_dict), dtype=np.int)
+    tp = np.zeros(1 + len(punctuation_dict), dtype=np.int)
+    fp = np.zeros(1 + len(punctuation_dict), dtype=np.int)
+    fn = np.zeros(1 + len(punctuation_dict), dtype=np.int)
     correct = 0
     total = 0
     test_loss = 0
@@ -93,18 +86,22 @@ def test(data_loader):
     tp[-1] = np.sum(tp[1:])
     fp[-1] = np.sum(fp[1:])
     fn[-1] = np.sum(fn[1:])
-    precision = tp/(tp+fp)
-    recall = tp/(tp+fn)
+    precision = tp / (tp + fp)
+    recall = tp / (tp + fn)
     f1 = 2 * precision * recall / (precision + recall)
 
-    return precision, recall, f1, correct/total
+    return precision, recall, f1, correct / total
 
 
-deep_punctuation.load_state_dict(torch.load(model_save_path))
-for i in range(len(test_loaders)):
-    precision, recall, f1, accuracy = test(test_loaders[i])
-    log = test_files[i] + '\n' + 'Precision: ' + str(precision) + '\n' + 'Recall: ' + str(recall) + '\n' + 'F1 score: ' + str(f1) + \
-          '\n' + 'Accuracy:' + str(accuracy) + '\n'
-    print(log)
-    with open(log_path, 'a') as f:
-        f.write(log)
+def run():
+    deep_punctuation.load_state_dict(torch.load(model_save_path))
+    for i in range(len(test_loaders)):
+        precision, recall, f1, accuracy = test(test_loaders[i])
+        log = test_files[i] + '\n' + 'Precision: ' + str(precision) + '\n' + 'Recall: ' + str(recall) + '\n' + \
+            'F1 score: ' + str(f1) + '\n' + 'Accuracy:' + str(accuracy) + '\n'
+        print(log)
+        with open(log_path, 'a') as f:
+            f.write(log)
+
+
+run()
