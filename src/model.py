1bd4a3d431c65e99b182f6484b78cb03775b26ad
xashru <xashru@gmail.com>
2020-06-17 22:00:02 +0600

    crf model log_likelihood and forward separated, lstm_dim added as argument

--- src/model.py
+++ src/model.py
@@ -5,7 +5,7 @@ from torchcrf import CRF
 
 
 class DeepPunctuation(nn.Module):
-    def __init__(self, pretrained_model, freeze_bert=False):
+    def __init__(self, pretrained_model, freeze_bert=False, lstm_dim=-1):
         super(DeepPunctuation, self).__init__()
         self.output_dim = len(punctuation_dict)
         self.bert_layer = MODELS[pretrained_model][0].from_pretrained(pretrained_model)
@@ -14,8 +14,12 @@ class DeepPunctuation(nn.Module):
             for p in self.bert_layer.parameters():
                 p.requires_grad = False
         bert_dim = MODELS[pretrained_model][2]
-        self.lstm = nn.LSTM(input_size=bert_dim, hidden_size=bert_dim, num_layers=1, bidirectional=True)
-        self.linear = nn.Linear(in_features=bert_dim * 2, out_features=len(punctuation_dict))
+        if lstm_dim == -1:
+            hidden_size = bert_dim
+        else:
+            hidden_size = lstm_dim
+        self.lstm = nn.LSTM(input_size=bert_dim, hidden_size=hidden_size, num_layers=1, bidirectional=True)
+        self.linear = nn.Linear(in_features=hidden_size*2, out_features=len(punctuation_dict))
 
     def forward(self, x, attn_masks):
         if len(x.shape) == 1:
@@ -34,36 +38,21 @@ class DeepPunctuation(nn.Module):
 class DeepPunctuationCRF(nn.Module):
     def __init__(self, pretrained_model, freeze_bert=False):
         super(DeepPunctuationCRF, self).__init__()
-        self.output_dim = len(punctuation_dict)
-        self.bert_layer = MODELS[pretrained_model][0].from_pretrained(pretrained_model)
-        # Freeze bert layers
-        if freeze_bert:
-            for p in self.bert_layer.parameters():
-                p.requires_grad = False
-        bert_dim = MODELS[pretrained_model][2]
-        self.lstm = nn.LSTM(input_size=bert_dim, hidden_size=bert_dim, num_layers=1, bidirectional=True)
-        self.linear = nn.Linear(in_features=bert_dim * 2, out_features=len(punctuation_dict))
+        self.bert_lstm = DeepPunctuation(pretrained_model, freeze_bert)
         self.crf = CRF(len(punctuation_dict), batch_first=True)
+
+    def log_likelihood(self, x, attn_masks, y):
+        x = self.bert_lstm(x, attn_masks)
+        attn_masks = attn_masks.byte()
+        return -self.crf(x, y, mask=attn_masks, reduction='token_mean')
 
     def forward(self, x, attn_masks, y):
         if len(x.shape) == 1:
             x = x.view(1, x.shape[0])  # add dummy batch for single sample
-        # (B, N, E) -> (B, N, E)
-        x = self.bert_layer(x, attention_mask=attn_masks)[0]
-        # (B, N, E) -> (N, B, E)
-        x = torch.transpose(x, 0, 1)
-        x, (_, _) = self.lstm(x)
-        # (N, B, E) -> (B, N, E)
-        x = torch.transpose(x, 0, 1)
-        x = self.linear(x)
-        # CRF requires byte tensor for mask
+        x = self.bert_lstm(x, attn_masks)
         attn_masks = attn_masks.byte()
-        crf_out = self.crf.forward(x, y, mask=attn_masks, reduction='token_mean')
-        y_pred = None
-        # pad list of integers to have equal length and convert to torch tensor
-        if not self.training:
-            dec_out = self.crf.decode(x, mask=attn_masks)
-            y_pred = torch.zeros(y.shape).long().to(y.device)
-            for i in range(len(dec_out)):
-                y_pred[i, :len(dec_out[i])] = torch.tensor(dec_out[i]).to(y.device)
-        return crf_out, y_pred
+        dec_out = self.crf.decode(x, mask=attn_masks)
+        y_pred = torch.zeros(y.shape).long().to(y.device)
+        for i in range(len(dec_out)):
+            y_pred[i, :len(dec_out[i])] = torch.tensor(dec_out[i]).to(y.device)
+        return y_pred
